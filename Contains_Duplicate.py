20cef3041095028f0cb75c2d676a0830aa673a20
vijay vignesh <97654626+vijayvignesh021@users.noreply.github.com>
2022-09-16 11:17:02 +0530

    Contains Duplicate

---
+++ Contains_Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        a= set()
+        for i in nums:
+            if i not in a:
+                a.add(i)
+            else:
+                return True
